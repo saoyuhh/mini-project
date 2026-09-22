9069cfa968eeb820a7458c67723def771f6d9d72
Hi <benisetiawan@Benis-MacBook-Pro.local>
2026-09-22 09:02:40 +0700

    perubahan style

--- src/app/about-event.tsx
+++ src/app/about-event.tsx
@@ -29,7 +29,7 @@ export function AboutEvent() {
       </Typography>
       <Typography
         variant="lead"
-        className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-gray-500"
+        className="mt-2 lg:max-w-4xl mb-8 w-full text-center font-normal !text-danger-500"
       >
         Welcome to the AI Conference 2023, where the future unfolds! Whether
         you&apos;re a seasoned AI professional, a curious newcomer, or a
